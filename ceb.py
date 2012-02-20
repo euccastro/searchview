daef83acb6329354fbb9c380e466d93160c8e146
Estevo <euccastro@gmail.com>
2012-02-20 18:39:23 -0500

    A* unidirectional search.e

--- ceb.py
+++ ceb.py
@@ -1,5 +1,20 @@
-# First version: just add, remove and drag points and edges around.
+"""
+Graph editor.
+
+To start with, you get a screen with only two vertices, the `start` vertex
+(marked with a right-facing triangle -- think of a 'Play' button) and the `end`
+vertex (marked with a tall rectangle -- think an end-of-proof/tombstone/Halmos mark).
+
+In general, actions affect the element closest to the mouse pointer. 
+
+Right click to add vertices, drag with left mouse button to move them, and drag with middle mouse button to create an edge connecting two vertices.
+
+Press Delete to delete the vertex closest to mouse cursor.  Press Backspace to delete the edge the midpoint of which is closest to the mouse cursor.
+"""
+
 from __future__ import division
+
+from heapq import heappush, heappop
 
 import stackless
 
@@ -28,8 +43,8 @@ goal = vec2((w.width * 2) // 3, (w.height * 2) // 3)
 
 vertices = [start, goal]
 
-start_poly = [vec2(x, y) for x, y in (-6, -10), (12, 0), (-6, 10)]
-goal_poly = [vec2(x, y) for x, y in (-6, -6), (6, -6), (6, 6), (-6, 6)]
+start_poly = [vec2(x, y) for x, y in (-5, -8), (8, 0), (-5, 8)]
+goal_poly = [vec2(x, y) for x, y in (-4, -8), (4, -8), (4, 8), (-4, 8)]
 
 edges = []
 
@@ -37,6 +52,8 @@ mouse_pos = None
 closest = None
 origin = None
 target = None
+optimal_path = None
+search_obsolete = False
 
 @w.event
 def on_mouse_motion(x, y, *etc):
@@ -64,6 +81,12 @@ def on_key_press(k, *etc):
 
 @w.event
 def on_draw():
+
+    global search_obsolete
+    if search_obsolete:
+        update_search()
+        search_obsolete = False
+
     glClear(GL_COLOR_BUFFER_BIT)
     glColor3f(*colors['cyan'])
     for point, poly in (start, start_poly), (goal, goal_poly):
@@ -80,6 +103,12 @@ def on_draw():
         glVertex2f(*origin)
         glVertex2f(*target)
     glEnd()
+    if optimal_path is not None:
+        glColor3f(*colors['green'])
+        glBegin(GL_LINE_STRIP)
+        for v in optimal_path:
+            glVertex2f(*v)
+        glEnd()
     glBegin(GL_POINTS)
     for v in vertices:
         if v is target:
@@ -98,6 +127,7 @@ def run():
     global closest
     global origin
     global target
+    global search_obsolete
     while True:
         evt = ch.receive()
         if evt.type == 'motion':
@@ -113,6 +143,7 @@ def run():
                     if newevt.type == 'motion':
                         closest.become(init_pos + newevt.pos - evt.pos)
                     elif newevt.type == 'release':
+                        search_obsolete = True
                         break
             elif evt.button == mouse.MIDDLE:
                 origin = closest_vertex(evt.pos)
@@ -126,7 +157,7 @@ def run():
                         vcopy = vertices[:]
                         vcopy.remove(origin)
                         dest = closest_vertex(newevt.pos, vcopy)
-                        edges.append((origin, dest))
+                        add_edge(origin, dest)
                         break
                     elif newevt.type == 'motion':
                         vcopy = vertices[:]
@@ -139,14 +170,27 @@ def run():
                     vertices.remove(closest)
                     for a, b in edges[:]:
                         if a is closest or b is closest:
-                            edges.remove((a, b))
+                            remove_edge(a, b)
                     closest = None
             elif evt.key == key.BACKSPACE:
                 if mouse_pos is not None:
                     d = dict([((a+b)/2, (a, b)) for a, b in edges])
                     closest_midpoint = closest_vertex(mouse_pos, d.keys())
-                    edges.remove(d[closest_midpoint])
+                    remove_edge(*d[closest_midpoint])
 
+def add_edge(a, b):
+    global search_obsolete
+    search_obsolete = True
+    edges.append((a, b))
+
+def remove_edge(a, b):
+    global search_obsolete
+    search_obsolete = True
+    edges.remove((a, b))
+
+def update_search():
+    global optimal_path
+    optimal_path = astar()
 
 def closest_vertex(p, vertices=vertices):
     return min(vertices, key=(lambda v: squaredist(v, p)))
@@ -159,6 +203,28 @@ colors = {'blue': (.0, .0, 1.),
           'red': (1., .0, .0),
           'teal': (.4, .8, .6),
           'cyan': (.0, .7, .7)}
+
+def astar():
+    visited = set()
+    frontier = [(0, 0, [start])]
+    while frontier:
+        heuristic, cost, path = heappop(frontier)
+        last = path[-1]
+        if last is goal:
+            return path
+        if last in visited:
+            continue
+        visited.add(last)
+        for a, b in edges:
+            if b is last:
+                a, b = b, a
+            if a is last and b not in visited:
+                new_cost = cost + (a-b).length()
+                estimate_to_goal = (goal-b).length()
+                heappush(frontier, 
+                         (new_cost+estimate_to_goal, 
+                          new_cost, 
+                          path + [b]))
 
 glClearColor(.5, .5, .5, 1.)
 glPointSize(4)
